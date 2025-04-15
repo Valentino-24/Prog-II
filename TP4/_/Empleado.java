da846369f129a2cbb71ea9b07afb19f4105dad21
Valentino <valentinolorca@gmail.com>
2025-04-15 13:39:55 -0300

    Cambiando salario int por double

--- TP4/_/Empleado.java
+++ TP4/_/Empleado.java
@@ -6,38 +6,6 @@ public class Empleado {
     private double salario;
     private static int totalEmpleados = 0;
 
-	public int getId() {
-		return this.id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return this.nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getPuesto() {
-		return this.puesto;
-	}
-
-	public void setPuesto(String puesto) {
-		this.puesto = puesto;
-	}
-
-	public double getSalario() {
-		return this.salario;
-	}
-
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
-// Constructores
 
     public Empleado(int id, String nombre, String puesto, double salario) {
         this.id = id;
@@ -48,11 +16,9 @@ public class Empleado {
     }
     
     public Empleado(String nombre, String puesto) {
-        this(totalEmpleados + 1, nombre, puesto, 1000.0); // ID automático y salario por defecto
+        this(totalEmpleados + 1, nombre, puesto, 1000.0);
     }
-    
-    
-    // Métodos sobrecargados para actualizar salario
+
     public void actualizarSalario(double porcentajeAumento) {
         this.salario += this.salario * (porcentajeAumento / 100);
     }
@@ -66,8 +32,7 @@ public class Empleado {
         return "ID: " + id + 
                "\nNombre: " + nombre + 
                "\nPuesto: " + puesto + 
-               "\nSalario: $" + String.format("%.2f", salario) + 
-               "\n---------------------";
+               "\nSalario: $" + String.format("%.2f", salario) ;
     }
     public static int mostrarTotalEmpleados() {
         return totalEmpleados;
--- TP4/_/Main.java
+++ TP4/_/Main.java
@@ -1,20 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Crear empleados usando diferentes constructores
-        Empleado emp1 = new Empleado(1, "Juan Pérez", "Desarrollador", 1500.0);
-        Empleado emp2 = new Empleado("María García", "Diseñadora");
-        Empleado emp3 = new Empleado("Carlos López", "Analista");
+
+        Empleado emp1 = new Empleado(1, "Enzo Pérez", "Desarrollador", 1500.0);
+        Empleado emp2 = new Empleado("Manuel Lanzini", "Diseñador");
+        Empleado emp3 = new Empleado("Ignacio Fernandez", "Analista");
         
-        // Actualizar salarios usando métodos sobrecargados
-        emp1.actualizarSalario(10); // Aumento del 10%
-        emp2.actualizarSalario(200); // Aumento fijo de 200
-        
-        // Mostrar información de los empleados
+       
+        emp1.actualizarSalario(10.0);
+        emp2.actualizarSalario(200); 
+    
         System.out.println(emp1.toString());
         System.out.println(emp2.toString());
         System.out.println(emp3.toString());
-        
-        // Mostrar total de empleados
+
         System.out.println("Total de empleados: " + Empleado.mostrarTotalEmpleados());
     }
 }
